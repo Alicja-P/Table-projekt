1b78be53e6431f8d2772c01e624bb044ec7930f2
Alicja-P <56272127+Alicja-P@users.noreply.github.com>
2019-11-02 20:59:31 +0100

    Update Main.java
    Poprawa, dodanie JPanel

--- Main.java
+++ Main.java
@@ -3,13 +3,17 @@ package table_try;
 
 import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame obj = new JFrame();
+		JPanel pan = new JPanel();
 		Gameplay gameplay = new Gameplay();
 		obj.setBounds(0, 0, 1280, 1024);
-		obj.setBackground(Color.DARK_GRAY);
+		pan.setBounds(0, 0, 1280, 1024);
+		obj.add(pan);
+		pan.setBackground(Color.DARK_GRAY);
 		obj.setTitle("Table");
 		obj.setResizable(false);
 		obj.setVisible(true);
