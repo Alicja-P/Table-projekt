35bab0ab9fda184b4ef1e5d4f7ab9a667b567592
Alicja-P <56272127+Alicja-P@users.noreply.github.com>
2019-12-27 15:30:54 +0100

    Update Gameplay.java
    PO ZAZNACZENIU ZMIENIA SIĘ LICZBA, MOŻLIWOŚĆ PRZEJŚCIA NA KOLEJNY POZIOM I POWRÓT DO MENU

--- Gameplay.java
+++ Gameplay.java
@@ -1,4 +1,4 @@
-package table_try;
+package table;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -10,19 +10,25 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import java.util.ArrayList;
+import java.util.Random;
+
+
 
 public class Gameplay extends JPanel implements ActionListener, MouseMotionListener, MouseListener{
 	
 	private TableGenerator map;
 	private Menu menu;
-	private int a = 10;
+	private Number number;
+	public int round = 1;
+	
+	//Random rand = new Random();
 	
 	
-	private int x, y, x1, y1, x2, y2;
+	private int x, y, x1, y1, x2, y2;						// współrzędne kursora przy przeciąganiu po kafelkach, przesuwaniu w menu i klikaniu w menu
 	ArrayList<Point> points = new ArrayList<Point>();
 	ArrayList<Point> points1 = new ArrayList<Point>();
 	ArrayList<Point> points2 = new ArrayList<Point>();
@@ -30,14 +36,15 @@ public class Gameplay extends JPanel implements ActionListener, MouseMotionListe
 	private Timer timer;
 	private int delay = 8;
 	
-	private int b,c;
-	public int game = 0;
+	private int b=0,c=0;	// zmienne do wyświetlania wyniku
+	public int game = 0;	// menu/gra/tryby
 	
 	
 	public Gameplay() {
-		map = new TableGenerator(a,a);
+		map = new TableGenerator(10,10);
 		menu = new Menu();
-		
+		number = new Number(round,10);
+		number.pick();					//Wybieranie liczby do zaznaczenia
 		addMouseMotionListener(this);
 		addMouseListener(this);
 		setFocusable(true);
@@ -50,22 +57,27 @@ public class Gameplay extends JPanel implements ActionListener, MouseMotionListe
 	
 	public void paint(Graphics g) {
 		if(game==0) {
-			menu.draw((Graphics2D)g);
+			menu.draw((Graphics2D)g);		//tryb menu
 		}
 		else {
-		//pisanie polecenia
-		
-		g.setColor(Color.black);
-		g.fillRect(0, 0, 1280, 1024);
-		//g.fillRect(790, 160, 400, 50);
-		g.setColor(Color.white);
-		g.setFont(new Font("serif", Font.BOLD, 40));
-		g.drawString("Zaznacz 12 kratek!", 800, 85);
-		
-		g.drawString(b+" * "+c+" = "+(b*c), 800, 190);
-		
-		map.draw((Graphics2D)g);
-		
+			//pisanie polecenia
+			g.setColor(Color.black);
+			g.fillRect(0, 0, 1280, 1024);
+			g.setColor(Color.white);
+			g.setFont(new Font("serif", Font.BOLD, 40));
+			//pisanie wyniku
+			g.drawString(b+" * "+c+" = "+(b*c), 800, 190);
+			//opcja kolejny poziom
+			g.setColor(Color.gray);
+			g.fillRect(900, 420, 330, 60);
+			g.fillRect(900, 520, 330, 60);
+			g.setColor(Color.white);
+			g.setFont(new Font("serif", Font.BOLD, 40));
+			g.drawString("Kolejny poziom", 920, 460);
+			g.drawString("Powrót do menu", 920, 560);
+			
+			map.draw((Graphics2D)g);
+			number.draw((Graphics2D)g);
 		}
 		g.dispose();
 	}
@@ -78,7 +90,7 @@ public class Gameplay extends JPanel implements ActionListener, MouseMotionListe
 		timer.start();
 		for(int i = 0; i<map.map.length; i++) {
 			for(int j = 0; j<map.map[0].length; j++) {
-				if( x > (j*map.width+80) && y > (i*map.height+50) )
+				if( x > (j*map.width+80) && y > (i*map.height+50) )		//wartość elementu (pokolorowany lub nie) w zależności od pozycji kursora
 				{
 					map.map[i][j] = 0;
 					b=i+1;
@@ -95,7 +107,7 @@ public class Gameplay extends JPanel implements ActionListener, MouseMotionListe
 		
 		for(int i=0; i<menu.menu.length; i++)
 		{
-			if(x1>450 && x1<750 && y1>(150 +i*100) && y1<(220 + i*100))
+			if(x1>450 && x1<750 && y1>(150 +i*100) && y1<(220 + i*100))		//zaznaczanie przycisku menu gdy kursor nad nim się przesuwa
 			{
 				menu.menu[i] = 1;
 			}
@@ -105,11 +117,28 @@ public class Gameplay extends JPanel implements ActionListener, MouseMotionListe
 		
 		for(int i=0; i<menu.menu.length; i++)
 		{
-			if(x2>450 && x2<750 && y2>(150+menu.f) && y2<(220+menu.f))
+			if(x2>450 && x2<750 && y2>(150+menu.f) && y2<(220+menu.f))		//wybranie opcji menu po kliknięciu myszką w przycisk
 			{
-				game =1;
+				game = 1;
 			}
 		}
+		
+		if(x2>900 && x2<1200 && y2>420 && y2<480)		//wybranie opcji "kolejny poziom"
+		{
+			number.row+=1;
+			x2 = 0;
+			y2 = 0;
+			
+		}
+		if(x2>900 && x2<1200 && y2>520 && y2<580)		//wybranie opcji "kolejny poziom"
+		{
+			game = 0;
+			x2 = 0;
+			y2 = 0;
+			
+		}
+		
+		
 		
 		repaint();
 		
@@ -164,10 +193,12 @@ public class Gameplay extends JPanel implements ActionListener, MouseMotionListe
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
+		if(b*c == number.number)
+		{
+			number.pick();
+		}
 		
 	}
 
 	
-
 }
