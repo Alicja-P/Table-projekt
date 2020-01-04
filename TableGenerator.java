e5a1c4f06a960574f1433b4a77ed32d9d3a6e999
Alicja-P <56272127+Alicja-P@users.noreply.github.com>
2020-01-04 13:56:08 +0100

    Update TableGenerator.java
    Dodatkowe funkcje: blanktable robi wszystkie kratki białe, blacktable zmienia rozmiar planszy

--- TableGenerator.java
+++ TableGenerator.java
@@ -3,59 +3,104 @@ package table_try;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.util.Random;
 
-public class TableGenerator {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TableGenerator extends JPanel{
 	
 	public int map[][];
 	public int width = 60;
 	public int height = 60;
-
-	public TableGenerator(int row, int col) {
-		map = new int[row][col];
+	public int column,row;
+	
+	Random rand = new Random();
+	
+	
+	
+	public TableGenerator(int a) {
+		map = new int[a][a];
+		blanktable();
+	}
+	
+	
+	
+	public void draw(Graphics2D g,int b) {
+		
+		for(int i=0; i<map.length; i++) {
+			for(int j=0; j<map[0].length; j++) {
+				
+				if(map[i][j]==1) {
+					g.setColor(Color.white);
+				}
+				if(map[i][j]==0)
+				{
+					g.setColor(Color.blue);
+				}
+				if(map[i][j]==3)
+				{
+					g.setColor(Color.black);
+				}
+				
+				g.fillRect(j * width + 80, i * height + 50, width-4, height-4);
+				
+				if(b==1)
+				{
+				int d=0;
+				
+				g.setFont(new Font("serif", Font.BOLD, 40));
+				
+				for(int z=0; z<map[0].length; z++)
+				{	
+					if(map[0][z]==0)
+						{g.setColor(Color.blue);
+						g.drawString("  "+(z+1), 80+d, 40);}
+					else
+						{g.setColor(Color.white);
+						g.drawString("  "+(z+1), 80+d, 40);}
+					d = d+59;
+				}
+				
+				
+			
+				
+					if(map[i][0]==0)
+						{g.setColor(Color.blue);
+						g.drawString((i+1)+"  ", 40, i * height + 85);}
+					else
+						{g.setColor(Color.white);
+						g.drawString((i+1)+"  ", 40, i * height + 85);}
+				}
+			}
+		}
+	}
+	
+	public void blanktable()
+	{
 		for(int i=0; i<map.length; i++) {
 			for(int j=0; j<map[0].length; j++) {
 				map[i][j] = 1;
 			}
 		}
 	}
-	
-	public void draw(Graphics2D g) {
-		g.setFont(new Font("serif", Font.BOLD, 30));
-		
+	public void blacktable()
+	{
 		for(int i=0; i<map.length; i++) {
 			for(int j=0; j<map[0].length; j++) {
-				if(map[i][j]>0) {
-					g.setColor(Color.white);
-				}
-				else
-				{
-					g.setColor(Color.blue);
-				}
-				
-				g.fillRect(j * width + 80, i * height + 50, width-4, height-4);
-				
-				int d=0;
-				
-				for(int z=0; z<map[0].length; z++)
-				{	
-					if(map[0][z]==0)
-						{g.setColor(Color.blue);
-						g.drawString("  "+(z+1), 90+d, 40);}
-					else
-						{g.setColor(Color.white);
-						g.drawString("  "+(z+1), 90+d, 40);}
-					d = d+59;
-				}
-			
-				
-					if(map[i][0]==0)
-						{g.setColor(Color.blue);
-						g.drawString((i+1)+"  ", 45, i * height + 85);}
-					else
-						{g.setColor(Color.white);
-						g.drawString((i+1)+"  ", 45, i * height + 85);}
+				map[i][j] = 3;
 			}
 		}
+		for(int i=0;i<1;i++) {
+			column = rand.nextInt(5)+3;
+			row = rand.nextInt(5)+3;}
+			for(int k=0;k<column;k++) {
+				for(int r=0;r<row;r++) {
+					map[k][r] = 1;
+				}
+			}	
+			
 	}
-	
+
 }
