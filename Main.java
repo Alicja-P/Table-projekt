46733e70684df30c0cfaf1fc724eddb5b313f81a
Alicja-P <56272127+Alicja-P@users.noreply.github.com>
2020-01-05 22:03:02 +0100

    Update Main.java
    Zmiana rozmiaru okna

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 package table_try;
 
-
 import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame obj = new JFrame();
 		JPanel pan = new JPanel();
 		Gameplay gameplay = new Gameplay();
-		obj.setBounds(0, 0, 1280, 1024);
-		pan.setBounds(0, 0, 1280, 1024);
+		
+		obj.setBounds(0, 0, 1280, 730);
 		obj.add(pan);
-		pan.setBackground(Color.DARK_GRAY);
+		pan.setBackground(Color.black);
 		obj.setTitle("Table");
 		obj.setResizable(false);
 		obj.setVisible(true);
