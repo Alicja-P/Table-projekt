563948d994280686d92cc72d7395922137cf7a3b
Alicja-P <56272127+Alicja-P@users.noreply.github.com>
2020-01-05 22:08:50 +0100

    Update Number.java

--- Number.java
+++ Number.java
@@ -12,39 +12,43 @@ public class Number {
 	private int tab[][];
 	public int number;
 	public int row=0;
-	private int spr;
-	private int s = 0;
-	
 
-	public Number(int a, int b)
+	public Number(int a)
 	{
 		row=a;
-		tab = new int[][] {
+		this.tab = new int[][] {
 			{4,8,9,12,16,14,18,20},
-			{5,8,9,10,15,20,25,28},
-			{6,12,14,16,18,24,30,36},
-			{7,14,15,20,21,25,28,35},
+			{32,6,9,10,15,20,25,28},
+			{8,12,14,16,18,24,30,36},
+			{45,27,15,20,21,25,28,35},
 			{42,32,40,56,64,24,30,36},
 			{27,42,48,56,45,54,63,72},
-			{50,60,70,80,90,63,68,81}
+			{50,60,70,80,90,63,64,81},
+			{56,28,72,32,42,63,90,81}
 		};
 	}
 	
-	public void pick(int row)
+	public void pick(int row)		//losowanie liczby z tablicy w zależności od poziomu
 	{
-		
-		for(int i=0; i<1;i++) {
-		int pickcolumn = rand.nextInt(7);
-		//int pickrow = rand.nextInt(row);
-		number = tab[row][pickcolumn];}
-	
-	
+		if(row<8) 	
+		{
+			for(int i=0; i<1;i++) 
+			{
+				int pickcolumn = rand.nextInt(7);
+				number = tab[row][pickcolumn];
+			}
+		}
+		else		//w 9 poziomie liczby losowane z całej tablicy
+		{
+			int pickcolumn = rand.nextInt(7);
+			int pickrow = rand.nextInt(7);
+			number = tab[pickrow][pickcolumn];
+		}
+	}
 	
 	public void draw(Graphics2D g) {
-		
 		g.setColor(Color.white);
 		g.setFont(new Font("serif", Font.BOLD, 40));
 		g.drawString("Liczba do zaznaczenia: " +number, 800, 85);
-		//g.setFont(new Font("serif", Font.BOLD, 30));
 	}
 }
